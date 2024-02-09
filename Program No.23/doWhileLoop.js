86cbb51808a24edee24b7ee6791aab604b99b30a
Mir hunza <130244140+Mir-Hunza@users.noreply.github.com>
2024-02-09 16:35:08 +0500

    Add files via upload

---
+++ Program No.23/doWhileLoop.js
@@ -0,0 +1,7 @@
+// Question No.22 "Describe the differences between the for loop, while loop, and do...while loop in JavaScript. When might you use each?"
+
+let i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
---
+++ Program No.23/whileLoop.js
@@ -0,0 +1,7 @@
+// Question No.22 "Describe the differences between the for loop, while loop, and do...while loop in JavaScript. When might you use each?"
+
+let i = 0;
+while (i < 5) {
+  console.log(i);
+  i++;
+}
