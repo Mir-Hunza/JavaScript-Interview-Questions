b1a1a41764ee51b9ee9cd148003fd68f94b8870c
Mir hunza <130244140+Mir-Hunza@users.noreply.github.com>
2024-02-29 23:43:25 +0500

    Add files via upload

---
+++ Program No.41/index.js
@@ -0,0 +1,8 @@
+// Question No.41 "Given an array of integers, use the `map` method to square each element and return a new array with the squared values."
+
+let originalArray = [1, 2, 3, 4, 5];
+
+let squaredArray = originalArray.map(num => num * num);
+
+console.log('Original Array:', originalArray);
+console.log('Squared Array:', squaredArray);
