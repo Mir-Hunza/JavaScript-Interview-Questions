83edb2eaa08a04b16de7618a2ef8682611b22931
Mir hunza <130244140+Mir-Hunza@users.noreply.github.com>
2024-03-07 19:59:04 +0500

    Add files via upload

---
+++ Program No.46/index.js
@@ -0,0 +1,19 @@
+// Question No.46 "Create a chain of array methods to find the average of all positive numbers in an array of mixed integers and return the result rounded to two decimal places."
+
+let mixedArray = [5, -2, 10, 8, -3, 7, -1, 4];
+
+let averageOfPositiveNumbers = mixedArray
+  .filter(num => num > 0) 
+  .reduce((sum, num, index, array) => {
+    sum += num;
+    if (index === array.length - 1) {
+
+      return sum / array.length;
+    } else {
+      return sum;
+    }
+  }, 0);
+
+let roundedResult = averageOfPositiveNumbers.toFixed(2);
+
+console.log('Average of Positive Numbers:', roundedResult);
